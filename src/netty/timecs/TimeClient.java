7e618c346ec8839534cd2521012cebd6fcc22517
liyubin <lixin58688@163.com>
2017-01-06 16:54:51 +0800

    新增使用netty实现时间服务器及客户端

---
+++ src/netty/timecs/TimeClient.java
@@ -0,0 +1,42 @@
+package netty.timecs;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TimeClient {
+
+	public static void main(String[] args) throws InterruptedException {
+		int port=8080;
+		new TimeClient().connect(port,"127.0.0.1");
+	}
+
+	private void connect(int port, String host) throws InterruptedException {
+		//配置客户端NIO线程组
+		EventLoopGroup group=new NioEventLoopGroup();
+		try{
+			Bootstrap b=new Bootstrap();
+			b.group(group)
+			 .channel(NioSocketChannel.class)
+			 .option(ChannelOption.TCP_NODELAY, true)
+			 .handler(new ChannelInitializer<SocketChannel>(){
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new TimeClientHandler());
+				}
+			 	});
+			//发起异步连接操作
+			ChannelFuture f=b.connect(host,port).sync();
+			//等待客户端链路关闭
+			f.channel().closeFuture().sync();
+		}finally{
+			group.shutdownGracefully();
+		}
+	}
+
+}
